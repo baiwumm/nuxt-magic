6500c748f60865e70cf44c4a76d6fc387ff21945
baiwumm <me@baiwumm.com>
2025-11-15 11:55:31 +0800

    feat: 初始化项目

---
+++ nuxt.config.ts
@@ -0,0 +1,46 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@vueuse/nuxt'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }, // 页面过渡效果
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.baiwumm.com/fonts/MapleMono-CN-Regular/result.css'
+        }
+      ]
+    }
+  },
+
+  css: ['~/assets/css/main.css'],
+  // @nuxt/ui 配置
+  ui: {
+    fonts: false
+  },
+
+  routeRules: {
+    '/api/**': {
+      cors: true
+    }
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
